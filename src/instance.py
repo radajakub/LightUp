88dcc0965363559beac78a2421cd24aad3cdc0fe
Jakub Rada <dev.jakubrada@icloud.com>
2023-11-06 17:39:52 +0100

    Add some methods to instance

--- src/instance.py
+++ src/instance.py
@@ -1,22 +1,38 @@
 import numpy as np
+from typing import Callable
 import os
+import itertools
 
 DATA_PATH = "data/"
 
 EMPTY = 'W'
 WALL = 'B'
 BULB = 'L'
+ILLUMINATED = '*'
 NUMBERS = ['0', '1', '2', '3', '4']
+DIFFS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 
 def _remove_endl(s: str) -> str:
     return s[:-1] if s[-1] == '\n' else s
 
 
+def _add_pos(p1: tuple[int, int], p2: tuple[int, int]) -> tuple[int, int]:
+    return tuple(map(sum, zip(p1, p2)))
+
+
 class Instance:
+    @staticmethod
+    def show(map: np.ndarray) -> None:
+        print('\n')
+        for row in map:
+            print(''.join(row), end='\n')
+        print('\n')
+
     def __init__(self, name: str) -> None:
         self._load_from_file(name)
         self._build()
+        self.reset()
 
     def __str__(self):
         res = f'Input: {self.input}\n'
@@ -24,11 +40,11 @@ class Instance:
             res += f'Expected output: {self.output}'
         return res
 
+    def print_orig_map(self) -> None:
+        Instance.show(self.orig_map)
+
     def print_map(self) -> None:
-        print('\n')
-        for row in self.map:
-            print(''.join(row), end='\n')
-        print('\n')
+        Instance.show(self.map)
 
     def _load_from_file(self, name: str) -> None:
         with open(os.path.join(DATA_PATH, f'{name}.txt'), 'r') as f:
@@ -38,38 +54,62 @@ class Instance:
             self.input = _remove_endl(lines[0])
             self.output = _remove_endl(lines[1]) if len(lines) > 1 else None
 
-    def _build(self):
+    def _build(self) -> None:
         self.n = int(np.sqrt(len(self.input)))
-        self.map = np.array(list(self.input)).reshape(self.n, self.n)[::-1]
+        self.orig_map = np.array(list(self.input)).reshape(self.n, self.n)[::-1]
 
-    def place_bulb(self, bulb: tuple[int, int]) -> None:
-        y, x = bulb
-        self.map[y, x] = BULB
-
-    def place_char(self, pos: tuple[int, int], char: str) -> None:
+    def _place_char(self, pos: tuple[int, int], char: str) -> None:
         self.map[pos[0], pos[1]] = char
 
-    def neighborhood(self, bulb: tuple[int, int]) -> list[tuple[int, int]]:
-        y, x = bulb
-        return [pos for pos in [(y, x + 1), (y, x - 1), (y + 1, x), (y - 1, x)]
-                if self.map[pos[0], pos[1]] == EMPTY]
+    def _is_empty(self, pos: tuple[int, int]) -> bool:
+        return self.map[pos[0], pos[1]] == EMPTY
 
-    def _get_line(self, start: tuple[int, int], d: tuple[int, int]):
-        y, x = start
-        dy, dx = d
+    def _is_number(self, pos: tuple[int, int]) -> bool:
+        return self.map[pos[0], pos[1]] in NUMBERS
+
+    def _is_bulb(self, pos: tuple[int, int]) -> bool:
+        return self.map[pos[0], pos[1]] == BULB
+
+    def _is_in_range(self, pos: tuple[int, int]) -> bool:
+        return 0 <= pos[0] < self.n and 0 <= pos[1] < self.n
+
+    def _get_line(self, pos: tuple[int, int], d: tuple[int, int]) -> list[tuple[int, int]]:
         line = []
-        while 0 <= y < self.n and 0 <= x < self.n and self.map[y, x] == EMPTY:
-            line.append((y, x))
-            y += dy
-            x += dx
+        pos = _add_pos(pos, d)
+        while self._is_in_range(pos) and self._is_empty(pos):
+            line.append(pos)
+            pos = _add_pos(pos, d)
         return line
 
+    def _get_positions(self, method: Callable[[tuple[int, int]], bool]) -> list[tuple[int, int]]:
+        return [(y, x)
+                for y in range(self.n)
+                for x in range(self.n)
+                if method((y, x))]
+
+    def reset(self) -> None:
+        self.map = self.orig_map.copy()
+
+    def place_bulb(self, bulb: tuple[int, int]) -> None:
+        self._place_char(bulb, BULB)
+        for illuminated in self.get_illuminated(bulb):
+            if not self._is_bulb(illuminated):
+                self._place_char(illuminated, ILLUMINATED)
+
+    def get_neighborhood(self, bulb: tuple[int, int]) -> list[tuple[int, int]]:
+        # y, x = bulb
+        return [pos for pos in [_add_pos(bulb, d) for d in DIFFS]
+                if self._is_empty(pos)]
+
+    def get_empty(self) -> list[tuple[int, int]]:
+        return self._get_positions(self._is_empty)
+
+    def get_numbers(self) -> list[tuple[int, int]]:
+        return self._get_positions(self._is_number)
+
     def get_illuminated(self, bulb: tuple[int, int]) -> list[tuple[int, int]]:
-        right = self._get_line(bulb, (0, 1))
-        left = self._get_line(bulb, (0, -1))
-        down = self._get_line(bulb, (1, 0))
-        up = self._get_line(bulb, (-1, 0))
-        return right + left + down + up
+        dirs = [[bulb]] + [self._get_line(bulb, d) for d in DIFFS]
+        return list(itertools.chain.from_iterable(dirs))
 
     def to_string(self) -> str:
         return ''.join(self.map[::-1].flatten())
--- src/run.py
+++ src/run.py
@@ -3,4 +3,11 @@ from instance import Instance
 if __name__ == '__main__':
     instance = Instance('01')
     print(instance)
+
     instance.print_map()
+    print(instance.get_empty())
+
+    # instance.place_bulb((4, 2))
+    # instance.print_map()
+    # instance.place_bulb((0, 2))
+    # instance.print_map()
